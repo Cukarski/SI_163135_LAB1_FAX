6b51e6f05a1a3a47acad40286dcdcd8f8c827592
Cukarski <49061668+Cukarski@users.noreply.github.com>
2019-04-15 23:33:09 +0200

    printMyName implemented

--- si_163135_lab1/program.cpp
+++ si_163135_lab1/program.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout<<"Vlatko Chukarski" ;
+
 }
 
 void printMyIndex () {
 	//TODO
+	cout<<" 163135";
 }
 
 int main () {
 	//TODO
+	printMyName();
+
+	printMyIndex();
+
 }
